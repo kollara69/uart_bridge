cb80cb5f4d5bde607b14d1b5348647bb4730ae26
kollara69 <76115733+kollara69@users.noreply.github.com>
2025-11-25 20:30:53 +0100

    Refactor UARTBridge constructor and cleanup loop

--- components/uart_bridge/uart_bridge.h
+++ components/uart_bridge/uart_bridge.h
@@ -13,34 +13,10 @@ class UARTBridge : public Component {
   text_sensor::TextSensor *sensor_mcu_to_display{nullptr};
   text_sensor::TextSensor *sensor_display_to_mcu{nullptr};
 
-  void setup() override {}
-
-  void loop() override {
-    uint8_t b;
-
-    // MCU → DISPLAY
-    while (uart1->available()) {
-      if (uart1->read_byte(&b)) {
-        if (sensor_mcu_to_display) {
-          sensor_mcu_to_display->publish_state(std::to_string(b));
-        }
-        uart2->write_byte(b);
-        ESP_LOGD("uart_bridge", "MCU->DISPLAY 0x%02X (%c)", b, isprint(b) ? b : '.');
-      }
-    }
-
-    // DISPLAY → MCU
-    while (uart2->available()) {
-      if (uart2->read_byte(&b)) {
-        if (sensor_display_to_mcu) {
-          sensor_display_to_mcu->publish_state(std::to_string(b));
-        }
-        uart1->write_byte(b);
-        ESP_LOGD("uart_bridge", "DISPLAY->MCU 0x%02X (%c)", b, isprint(b) ? b : '.');
-      }
-    }
+  // Konstruktor
+  UARTBridge(uart::UARTComponent *mcu, uart::UARTComponent *disp) {
+    uart1 = mcu;
+    uart2 = disp;
   }
-};
 
-}  // namespace uart_bridge
-}  // namespace esphome
+  void setup() override

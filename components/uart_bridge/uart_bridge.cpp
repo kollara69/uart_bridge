1996201fa0c6dc2a9e8b7a4c29ddca76800e955a
kollara69 <76115733+kollara69@users.noreply.github.com>
2025-11-25 19:12:27 +0100

    Create uart_bridge.cpp for build system compatibility
    Add uart_bridge.cpp to satisfy build system requirements.

--- components/uart_bridge/uart_bridge.cpp
+++ components/uart_bridge/uart_bridge.cpp
@@ -1,1 +1,10 @@
+#include "uart_bridge.h"
 
+namespace esphome {
+namespace uart_bridge {
+
+// No additional registration needed – all logic in h file.
+// This file exists to make ESPHome's build system happy.
+
+}  // namespace uart_bridge
+}  // namespace esphome
